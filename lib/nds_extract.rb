ed7af035d08347e043ef5e306d4eeea42f88bb24
Ben Miriello <benmiriello@gmail.com>
2019-11-07 01:27:49 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -37,3 +37,51 @@ require 'directors_database'
 # end
 # spielberg_gross
 # end
+
+def directors_totals(nds)
+
+  result = {}
+
+  dir_index = 0
+  count_outer = 0
+  index_outer = nds.length
+  
+  while count_outer < index_outer do
+    dir_gross = 0
+    count_inner = 0
+    index_inner = nds[dir_index][:movies].length
+    while count_inner < index_inner do
+      dir_gross += nds[dir_index][:movies][count_inner][:worldwide_gross]
+      count_inner += 1
+    end
+
+    puts dir_index
+    puts dir_gross
+    dir_index += 1
+    count_outer += 1
+
+    if dir_index == 1
+      result["Steven Spielberg"] = dir_gross
+    elsif dir_index == 2
+      result["Russo Brothers"] = dir_gross
+    elsif dir_index == 3
+      result["James Cameron"] = dir_gross
+    elsif dir_index == 4
+      result["Spike Lee"] = dir_gross
+    elsif dir_index == 5
+      result["Wachowski Siblings"] = dir_gross
+    elsif dir_index == 6
+      result["Robert Zemeckis"] = dir_gross
+    elsif dir_index == 7
+      result["Quentin Tarantino"] = dir_gross
+    elsif dir_index == 8
+      result["Martin Scorsese"] = dir_gross
+    elsif dir_index == 9
+      result["Francis Ford Coppola"] = dir_gross
+    end
+    
+  end
+  result
+end
+
+directors_totals(nds)
