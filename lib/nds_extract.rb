6b8303b0945ce118609442a12c0e0860abe0031c
Ben Miriello <benmiriello@gmail.com>
2019-11-07 01:27:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,39 +1,39 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
-def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-# result = {
-# }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
-  # nil
+# def directors_totals(nds)
+#   # Remember, it's always OK to pretty print what you get *in* to make sure
+#   # that you know what you're starting with!
+#   #
+#   #
+#   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
+# # result = {
+# # }
+#   #
+#   # Use loops, variables and the accessing method, [], to loop through the NDS
+#   # and total up all the
+#   # ...
+#   # ...
+#   # ...
+#   #
+#   #
+#   # Be sure to return the result at the end!
+#   # nil
   
-result = {}
+# result = {}
 
-spielberg_index = 0
+# spielberg_index = 0
 
 
-spielberg_gross = 0
-count = 0
-index = nds[spielberg_index][:movies].length
+# spielberg_gross = 0
+# count = 0
+# index = nds[spielberg_index][:movies].length
 
-while count < index do
-  movie_gross = nds[spielberg_index][:movies][0][:worldwide_gross]
-  spielberg_gross += movie_gross
-  puts spielberg_gross
-  count += 1
-end
-spielberg_gross
-end
+# while count < index do
+#   movie_gross = nds[spielberg_index][:movies][0][:worldwide_gross]
+#   spielberg_gross += movie_gross
+#   puts spielberg_gross
+#   count += 1
+# end
+# spielberg_gross
+# end
